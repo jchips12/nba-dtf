3c18749371beed8be0233cad97d54cbe979566a2
JC Hipolito <jchips12@gmail.com>
2017-03-03 23:05:45 +0800

    add authentication

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const authManager = require('../utils/authentication-manager');
 
 /* GET api listing. */
-router.get('/', (req, res) => {
+router.get('/', authManager.authenticate, (req, res) => {
     res.send('api works');
 });
 
---
+++ server/utils/authentication-manager.js
@@ -0,0 +1,19 @@
+var authenticate = function (req, res, next) {
+    var auth;
+    if (req.headers.authorization) {
+        auth = new Buffer(req.headers.authorization.substring(6), 'base64')
+            .toString().split(':');
+    }
+    if (!auth || auth[0] !== 'username' || auth[1] !== 'password') {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="BadEggsRealm"');
+        res.end('Unauthorized');
+    } else {
+        req.authenticated = true;
+        next();
+    }
+};
+
+module.exports = {
+    authenticate: authenticate
+};
